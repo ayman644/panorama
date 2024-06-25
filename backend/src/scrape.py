6d83a1bd1d4098a1af8a98036e91c24d10cd0d57
Dylan Colwill <dylan.colwill@hotmail.com>
2024-06-25 12:19:19 +1000

    small fixes and cleanup

--- backend/src/scrape.py
+++ backend/src/scrape.py
@@ -1,20 +1,20 @@
 import requests 
 from bs4 import BeautifulSoup
 
-def getSiteContent():
-    url = "https://www.theguardian.com/media/article/2024/jun/25/julian-assange-plea-deal-with-us-free-to-return-australia" 
+def getSiteText(url):
+    # url = "https://www.theguardian.com/media/article/2024/jun/25/julian-assange-plea-deal-with-us-free-to-return-australia" 
     response = requests.get(url)
 
     soup = BeautifulSoup(response.content, 'html.parser')
 
     text = soup.get_text()
 
-    print(text)
+    # print(text)
 
     siteContent =''
 
     paragraphs = soup.find_all('p')
     for paragraph in paragraphs:
-        paraText =paragraph.get_text()
+        paragraph =paragraph.get_text()
         siteContent +=paragraph
     return siteContent
