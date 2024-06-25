95988a607cced98ba7defb651687eee1149b05a7
Dylan Colwill <dylan.colwill@hotmail.com>
2024-06-25 11:54:44 +1000

    finished script

--- backend/src/scrape.py
+++ backend/src/scrape.py
@@ -1,14 +1,20 @@
 import requests 
 from bs4 import BeautifulSoup
-url = "https://www.theguardian.com/media/article/2024/jun/25/julian-assange-plea-deal-with-us-free-to-return-australia" 
-response = requests.get(url)
 
-soup = BeautifulSoup(response.content, 'html.parser')
+def getSiteContent():
+    url = "https://www.theguardian.com/media/article/2024/jun/25/julian-assange-plea-deal-with-us-free-to-return-australia" 
+    response = requests.get(url)
 
-text = soup.get_text()
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-print(text)
+    text = soup.get_text()
 
-paragraphs = soup.find_all('p')
-for paragraph in paragraphs:
-    print(paragraph.get_text())
+    print(text)
+
+    siteContent =''
+
+    paragraphs = soup.find_all('p')
+    for paragraph in paragraphs:
+        paraText =paragraph.get_text()
+        siteContent +=paragraph
+    return siteContent
